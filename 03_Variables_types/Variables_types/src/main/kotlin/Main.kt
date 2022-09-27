64080c784bd1a1be8bc879e8cd6432c750ae184b
Vladislav_Kizin <vadik.05@mail.ru>
2022-09-27 23:30:44 +0300

    Д.З. выполнено

--- 03_Variables_types/Variables_types/src/main/kotlin/Main.kt
+++ 03_Variables_types/Variables_types/src/main/kotlin/Main.kt
@@ -5,13 +5,10 @@ fun main() {
     var weight: Float = 98.5f
     val isChild = height < 1.5 || weight < 40
     println("Владислав ребёнок ? \n $isChild")
-    val info: String = "имя = "  + firstName + "\n" + "фамилия = " + lastName + "\n" +
-            "рост = " + height + "\n" + "вес = " + weight + "\n" +
-            "Владислав ребенок ?" + "\n" +  isChild
+    val info: String = "имя -  $firstName , фамилия - $lastName , рост - $height , вес - $weight , Владислав ребенок ? - $isChild "
     println(info)
-    height = 1.49
-    println("Новый рост = " + height)
-    println(info.replace("height", "height2"))
+    var height2: Double = 1.88
+    println(info.replace("$height", "$height2"))
 
 
 }
