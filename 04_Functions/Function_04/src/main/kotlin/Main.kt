57137b9f902fab41f6a4c5c1ee54cc374cbf2d99
Vladislav_Kizin <vadik.05@mail.ru>
2022-10-04 18:55:12 +0300

    У меня все таки получилось!

--- 04_Functions/Function_04/src/main/kotlin/Main.kt
+++ 04_Functions/Function_04/src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val fullString = "F2p)v\"y233{0->c}ttelciFc"
+    val firstString = fullString.take(12)
+    val secondString = fullString.takeLast(12)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val newFirstString = firstString.map {char -> char + 1}.joinToString("").replace("5", "s").replace("4", "u").map {char -> char - 3}.joinToString("").replace("0", "o")
+    val newSecondString = secondString.map {char -> char -4}.joinToString(" ").replace("_", " ")
+
+    println(newFirstString)
+
+
 }
