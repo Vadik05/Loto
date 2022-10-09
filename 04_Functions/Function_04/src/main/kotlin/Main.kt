bca933e6f910ca6c2a2f55ad58caa53d9eddf4d2
Vladislav_Kizin <vadik.05@mail.ru>
2022-10-09 04:05:11 +0300

    Сделано через функции

--- 04_Functions/Function_04/src/main/kotlin/Main.kt
+++ 04_Functions/Function_04/src/main/kotlin/Main.kt
@@ -3,10 +3,16 @@ fun main() {
     val firstString = fullString.take(12)
     val secondString = fullString.takeLast(12)
 
-    val newFirstString = firstString.map {char -> char + 1}.joinToString("").replace("5", "s").replace("4", "u").map {char -> char - 3}.joinToString("").replace("0", "o")
-    val newSecondString = secondString.map {char -> char -4}.joinToString(" ").replace("_", " ")
-
-    println(newFirstString + newSecondString)
-
-
+    println(newFirstString(firstString) + newSecondString(secondString))
 }
+
+
+
+fun newFirstString(firstString: String): String {
+    return firstString.map { char -> char + 1 }.joinToString("").replace("5", "s").replace("4", "u")
+        .map { char -> char - 3 }
+        .joinToString("").replace("0", "o")
+}
+fun newSecondString(secondString: String): String = secondString.reversed().map { char -> char - 4 }.joinToString("").replace("_", " ")
+
+
