ce1be831c2c1342db91e9bcec01422a4d48f874c
Vladislav_Kizin <vadik.05@mail.ru>
2022-10-25 07:37:36 +0300

    6 домашняя работа, добавил заполнение пары логин-пароль с клавиатуры

--- 06_Collections/Array/src/main/kotlin/HomeWork_Part2.kt
+++ 06_Collections/Array/src/main/kotlin/HomeWork_Part2.kt
@@ -2,24 +2,39 @@ import kotlin.random.Random
 import kotlin.random.nextInt
 
 fun main() {
-    val myHashMap = hashMapOf(
-        "user1" to "qwerty1",
-        "user2" to "qwerty2",
-        "user3" to "qwerty3"
-    )
+    val loginBase: HashMap<String, String> = hashMapOf()
+    val n = 3
+    println("Регистрация пользователей")
+    for (i in 1..n){
+        println("Введите логин")
+        val login: String? = readLine()
+        println("Введите пароль")
+        val password: String? = readLine()
+        if (password != null) {
+            loginBase.put(login.toString(), password)
+        }
+        loginBase[login] = password
+
+    }
+
     var b = true
     do {
+        println("Вход в учетную запись")
         println("Введите логин")
         val login = readLine().toString()
         println("Введите пароль")
         val password = readLine().toString()
-        if (myHashMap[login] == password) {
+        if (loginBase[login] == password) {
             println("Добро пожаловать $login")
             b = true
             break
         } else {
             println("Неверный логин или пароль")
         }
-    }while (myHashMap[login] != password)
+    }while (loginBase[login] != password)
+
+}
+
+private operator fun <K, V> HashMap<K, V>.set(login: V?, value: V?) {
 
 }
