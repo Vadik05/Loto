4d0299ad1a780775f6bd56a6b2cab6d29569ef45
Vladislav_Kizin <vadik.05@mail.ru>
2023-02-24 23:32:24 +0300

    Лото. Сделано для двух игроков. Выигрывает тот, кто первый закрыл всю карту.

--- 14_Flow/Loto/src/main/kotlin/Main.kt
+++ 14_Flow/Loto/src/main/kotlin/Main.kt
@@ -5,10 +5,58 @@ import kotlinx.coroutines.flow.asSharedFlow
 import java.util.Collections.shuffle
 
 
-fun main() {
-    val player1 = Player()
-    player1.getCard()
-    println(player1.card)
+suspend fun main() {
+    runBlocking {
+        launch {
+
+            val player1 = Player()
+            println("Карта первого игрока :")
+            player1.getCard()
+
+            val player2 = Player()
+            println("Карта второго игрока :")
+            player2.getCard()
+
+            var counter = 0
+            var coin1 = 0
+            var coin2 = 0
+
+            Generator.sharedFlow.collect {
+                println("Ход ${counter + 1}. Выпал номер $it")
+                counter++
+                for (row in player1.card) {
+                    for (cell in row) {
+                        if (it == cell) {
+                            coin1++
+                            println("Совпадение у первого игрока !")
+                            if (coin1 == 15) {
+                                println("Игрок 1 выиграл !")
+                                yield()
+                                cancel()
+                            }
+                        }
+                    }
+
+                }
+                for (row in player2.card) {
+                    for (cell in row) {
+                        if (it == cell) {
+                            coin2++
+                            println("Совпадение у второго игрока !")
+                            if (coin2 == 15) {
+                                println("Игрок 2 выиграл !")
+                                yield()
+                                cancel()
+                            }
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+    }
 
 }
 
@@ -28,7 +76,7 @@ object Generator {
                 shuffle(barrel)
                 for (it in 0 until barrel.size) {
                     sharedFlowOne.emit(barrel[it])
-                    delay(300)
+                    delay(1000)
                 }
                 scope.cancel()
             }
